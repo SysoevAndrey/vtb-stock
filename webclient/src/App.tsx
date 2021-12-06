898ba9758103420f172558a44251f8d491e11e68
Andrey Sysoev <andrey@sysoev.com>
2021-12-06 21:45:25 +0300

    added Auth page

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Auth from './pages/Auth';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className="title">Hello</h1>
+    <div className='App'>
+      <Auth />
     </div>
   );
 }
