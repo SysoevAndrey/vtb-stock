9a7bef62c93a3a256babca35f1f56b0d24e66a01
Andrey Sysoev <andrey@sysoev.com>
2021-12-06 22:10:23 +0300

    added Header component

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header';
 import Auth from './pages/Auth';
 import './App.scss';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
+      <Header />
       <Auth />
     </div>
   );
